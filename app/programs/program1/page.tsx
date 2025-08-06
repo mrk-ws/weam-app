0827ad2025ba4b5371c99d238fb9bb10b8743e3a
Your Name <you@example.com>
2025-08-06 08:42:39 +0300

    تعديل صفحة برنامج سوق

--- app/programs/program1/page.tsx
+++ app/programs/program1/page.tsx
@@ -47,45 +47,54 @@ const Program1 = () => {
       />
 
       {/* Program Details */}
-      <h1 className="text-3xl font-bold mt-4">برنامج دعم الأيتام</h1>
+      <h1 className="text-3xl font-bold my-4">برنامج سوق</h1>
       <p className="text-gray-600 dark:text-gray-200 mt-2">
-        يهدف البرنامج إلى تقديم الدعم المادي والمعنوي للأطفال الأيتام من خلال
-        توفير بيئة تعليمية وصحية مناسبة.
+        مبادرة شاملة لتمكين الأيتام اقتصاديًا من خلال تزويدهم بالمهارات والمعرفة
+        اللازمة لإنشاء مشاريعهم الخاصة وتحقيق الاستقلال المالي.
+      </p>
+      <h2 className="text-3xl font-bold mt-6">الفكرة</h2>
+      <p className="text-gray-600 dark:text-gray-200 mt-2">
+        تحويل التحديات التي يواجهها الأيتام إلى فرص، وتمكينهم من بناء مستقبل مهني مستدام.
       </p>
 
       {/* Goals */}
       <h2 className="text-2xl font-semibold mt-4">أهداف البرنامج</h2>
       <ul className="list-disc list-inside text-gray-700 dark:text-gray-200">
-        <li>تقديم الدعم التعليمي للأطفال الأيتام</li>
-        <li>تحسين الظروف المعيشية لهم</li>
-        <li>توفير الرعاية الصحية والنفسية</li>
+        <li>تدريب 50 يتيماً على إعداد دراسات جدوى لمشاريع صغيرة في مجالات مختلفة بحلول نهاية العام الأول</li>
+        <li>تقديم 30 خطة عمل قابلة للتطبيق للمشاريع الصغيرة بحلول نهاية العام الثاني</li>
+        <li>الحصول على تمويل لـ 20 مشروعاً صغيراً من مصادر مختلفة بحلول نهاية العام الثالث</li>
+        <li>تحقيق معدل نجاح 70% للمشاريع التي تم تمويلها بعد مرور عامين</li>
+        <li>زيادة متوسط دخل الأسر المشاركة بنسبة 15% خلال العام الثالث</li>
       </ul>
 
       {/* Target Audience */}
       <h2 className="text-2xl font-semibold mt-4">المستفيدون المستهدفون</h2>
       <p className="text-gray-700 dark:text-gray-200">
-        الأطفال الأيتام من الفئات الأكثر احتياجًا في المجتمع.
+        الأيتام من الجنسين الذين تتراوح أعمارهم بين 18 و22 ، والراغبين في بدء مشاريعهم الخاصة.
       </p>
 
       {/* Expected Impact */}
       <h2 className="text-2xl font-semibold mt-4">الأثر المتوقع</h2>
       <p className="text-gray-700 dark:text-gray-200">
-        تحسين مستوى التعليم والصحة للأطفال الأيتام، وتعزيز فرصهم في المستقبل.
+        تحسين مستوى المعيشة للأيتام، وزيادة فرصهم في الحصول على دخل مستدام, وزيادة عدد المشاريع الصغيرة التي يملكها ويقودها الأيتام, زيادة مساهمة الأيتام في الناتج المحلي الإجمالي وتعزيز روح المبادرة والابتكار لديهم.
       </p>
 
       {/* Implementation Steps */}
       <h2 className="text-2xl font-semibold mt-4">مراحل التنفيذ</h2>
       <ul className="list-decimal list-inside text-gray-700 dark:text-gray-200">
-        <li>تحديد الأطفال المستفيدين</li>
-        <li>توفير الدعم التعليمي والصحي</li>
-        <li>متابعة وتقييم الأثر</li>
+        <li>التخطيط: تحديد الأهداف، اختيار المشاركين، وضع خطة العمل.</li>
+        <li>التدريب: تقديم برامج تدريبية مكثفة في ريادة الأعمال وإعداد دراسات الجدوى.</li>
+        <li>تطوير الخطط: مساعدة المشاركين في تطوير خطط أعمال متكاملة.</li>
+        <li>التمويل: مساعدة المشاركين في الحصول على التمويل اللازم.</li>
+        <li>التشغيل: متابعة المشاريع وتقديم الدعم المستمر.</li>
+        <li>التقييم: تقييم أداء المبادرة وتعديل الخطط حسب الحاجة.</li>
       </ul>
 
       {/* Share Section */}
       <Link
-        href={"https://store.weam.org.sa/"}
+        href={"https://store.weam.org.sa/p/103530"}
         target="blank"
-        className="rounded-lg relative w-40 h-10 cursor-pointer flex items-center border border-primaryColor bg-primaryColor group hover:bg-primaryColor active:bg-primaryColor active:border-primaryColor overflow-hidden mx-auto"
+        className="rounded-lg relative w-40 h-10 cursor-pointer flex items-center border border-primaryColor bg-primaryColor group hover:bg-primaryColor active:bg-primaryColor active:border-primaryColor overflow-hidden my-4 md:my-2 mx-auto"
       >
         <span className="text-gray-200 font-semibold mr-12 transform group-hover:translate-x-20 transition-all duration-300">
           إدعم البرنامج
